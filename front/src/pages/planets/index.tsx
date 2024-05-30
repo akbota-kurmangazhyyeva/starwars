dae353a1e5d1ab08a83a37e7ee0e5ff4ea3a0d71
Akbota <aqbotamaratqyzy@gmail.com>
2024-05-31 00:54:00 +0500

    feat: routing

---
+++ front/src/pages/planets/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import axiosInstance from '../../utils/axiosInstance'
+
+const index = () => {
+    const [planets, setPlanets] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+
+    return (
+    <div>index</div>
+  )
+}
+
+export default index
